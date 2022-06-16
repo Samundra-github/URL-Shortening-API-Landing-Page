919fcdb7baefea47616aebeb7c1742b2d3db51f0
Samundra Bhandari <76542341+Samundra-github@users.noreply.github.com>
2022-06-16 14:07:06 +0545

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -2,16 +2,16 @@ let toogleIcon = document.querySelector("#toggle");
 let hideIcon = document.querySelector("#hide");
 hideIcon.style = "display:none";
 
-const navButton = document.querySelector(".header__buttons");
+const meueButton = document.querySelector(".header--buttons");
 
 const toggle = () => {
   toogleIcon.style = "display:none ;";
   hideIcon.style = "display:block";
-  navButton.style = "display:block";
+  meueButton.style = "display:block";
 };
 
 const hide = () => {
   toogleIcon.style = "display:block ;";
   hideIcon.style = "display:none";
-  navButton.style = "display:none";
+  meueButton.style = "display:none";
 };
